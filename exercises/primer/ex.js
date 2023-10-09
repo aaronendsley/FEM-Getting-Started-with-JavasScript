0b18585c91281299464a40c9b05af9ba58e22838
Aaron Endsley <aaronendsley@gmail.com>
2023-10-09 11:08:05 -0500

    first exercise

---
+++ exercises/primer/ex.js
@@ -0,0 +1,24 @@
+// TODO: define addFavoriteBook(..) function
+function addFavoriteBook(book) {
+  if (!book.toLowerCase().includes("great")) {
+    favoriteBooks.push(book);
+  }
+}
+// TODO: define printFavoriteBooks() function
+function printFavoriteBooks() {
+  console.log(`Favorite Books: ${favoriteBooks.length}`);
+  for (let book of favoriteBooks) {
+    console.log(book);
+  }
+}
+
+var favoriteBooks = [];
+
+addFavoriteBook("A Song of Ice and Fire");
+addFavoriteBook("The Great Gatsby");
+addFavoriteBook("Crime & Punishment");
+addFavoriteBook("Great Expectations");
+addFavoriteBook("You Don't Know JS");
+
+// TODO: print out favorite books
+printFavoriteBooks();
