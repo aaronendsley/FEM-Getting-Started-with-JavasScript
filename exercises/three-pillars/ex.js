a3c93e8b15dabeafd88f5dd3c328538b238117ea
Aaron Endsley <aaronendsley@gmail.com>
2023-10-11 17:21:15 -0500

    finished solution for last exercise

--- exercises/three-pillars/ex.js
+++ exercises/three-pillars/ex.js
@@ -1,43 +1,49 @@
 class Bookshelf {
-	constructor() {
-		this.favoriteBooks = [];
-	}
+  constructor() {
+    this.favoriteBooks = [];
+  }
 
-	// TODO: define methods `addFavoriteBook(..)`
-	// and `printFavoriteBooks()`
+  // TODO: define methods `addFavoriteBook(..)`
+  addFavoriteBook(bookName) {
+    if (!bookName.includes("Great")) {
+      this.favoriteBooks.push(bookName);
+    }
+  }
+
+  // and `printFavoriteBooks()`
+  printFavoriteBooks() {
+    console.log(`Favorite Books: ${String(this.favoriteBooks.length)}`);
+    for (let bookName of this.favoriteBooks) {
+      console.log(bookName);
+    }
+  }
 }
 
-function addFavoriteBook(bookName) {
-	if (!bookName.includes("Great")) {
-		favoriteBooks.push(bookName);
-	}
-}
-
-function printFavoriteBooks() {
-	console.log(`Favorite Books: ${favoriteBooks.length}`);
-	for (let bookName of favoriteBooks) {
-		console.log(bookName);
-	}
-}
-
-function loadBooks( /* .. */ ) {
-	// TODO: call fakeAjax( .. );
+function loadBooks(Bookshelf) {
+  // TODO: call fakeAjax( .. );
+  fakeAjax(BOOK_API, function bookCall(booknames) {
+    for (let book of booknames) {
+      Bookshelf.addFavoriteBook(book);
+    }
+    Bookshelf.printFavoriteBooks();
+  });
 }
 
 var BOOK_API = "https://some.url/api";
 
-
 // ***********************
 
 // NOTE: don't modify this function at all
-function fakeAjax(url,cb) {
-	setTimeout(function fakeLoadingDelay(){
-		cb([
-			"A Song of Ice and Fire",
-			"The Great Gatsby",
-			"Crime & Punishment",
-			"Great Expectations",
-			"You Don't Know JS"
-		]);
-	},500);
+function fakeAjax(url, cb) {
+  setTimeout(function fakeLoadingDelay() {
+    cb([
+      "A Song of Ice and Fire",
+      "The Great Gatsby",
+      "Crime & Punishment",
+      "Great Expectations",
+      "You Don't Know JS",
+    ]);
+  }, 500);
 }
+let shelf = new Bookshelf();
+loadBooks(shelf);
